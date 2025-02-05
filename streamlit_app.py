2d7b15d2284f218b2d3c2d20e08789cef2184de3
mzhub <mishabhadhi@gmail.com>
2025-02-05 17:15:16 +0530

    loan

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ with open("model.pkl", "rb") as f:
 age = st.slider("**Age**", 20, 144, 30)
 gender = st.radio("**Gender**", ("Male", "Female"))
 education = st.selectbox("**Education**", ('High school', 'Associate', 'Bachelor', 'Master', 'Doctorate'), placeholder = "Select your educational qualification", index = None)
-income = st.number_input("**Annual Income**", 8000, 7200000, help = "in dollars", value = None, placeholder = "Type your annual income")
+income = st.number_input("**Annual Income**", 8000, 7200000, help = "in dollars", placeholder = "Type your annual income")
 experience = st.number_input("**Employment experience**", 0, 125, help = "in years")
 ownership = st.selectbox("**House ownership**", ('Own', 'Mortage', 'Rent', 'Other'), placeholder ="Select your house ownership type", index = None)
 amount = st.number_input("**Requried loan amount**", 500, 35000, help = "in dollars")
