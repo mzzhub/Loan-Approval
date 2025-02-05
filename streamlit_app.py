f61a84e3e4240c518a7d0fd5277743c616ec380d
mzhub <mishabhadhi@gmail.com>
2025-02-05 15:41:32 +0530

    loan

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,16 @@ st.title('Loan Eligiblity Test')
 
 with open("model.pkl", "rb") as f:
     lrc = pickle.load(f)
-    
-st.write("AGE")
-age = st.slider("Age", 20, 144, 30)
 
+age = st.slider("Age", 20, 144, 30)
+gender = st.radio("Gender", ("Male", "Female"), placeholder = "Select your gender")
+education = st.selectbox("Education", ('High School', 'Associate', 'Bachelor', 'Master', 'Doctorate'), placeholder = "Select your educational qualification")
+# income = 
+# experiance = 
+# ownership = 
+# amount = 
+# purpose = 
+# rate = 
+# cbl = "Credit Bureau: Person's Credit History Length"
+# credit_score = 
+# defaults = "Previous Loan Defaults on File"
