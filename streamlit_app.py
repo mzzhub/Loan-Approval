5287ee9f09ea120e06510c4e03410750f0a39a84
mzhub <mishabhadhi@gmail.com>
2025-02-05 16:03:08 +0530

    loan

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,7 @@ income = st.number_input("Annual Income", 8000, 7200000)
 experience = st.number_input("Employment experience", 0, 125)
 ownership = st.selectbox("House ownership type", ('Own', 'Mortage', 'Rent', 'Other'))
 amount = st.number_input("Requried loan amount", 500, 35000, help = "in dollars")
-# purpose = 
+type = st.selectbox("Loan type", ('Education' 'Medical' 'Home improvement''Debt consolidation''Personal''Venture'))
 # rate = 
 # cbl = "Credit Bureau: Person's Credit History Length"
 # credit_score = 
