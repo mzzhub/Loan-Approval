9962d9411136ad752f23506ed612d829a7e4d529
mzhub <mishabhadhi@gmail.com>
2025-02-14 16:52:28 +0530

    updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,9 +5,11 @@ import matplotlib.pyplot as plt
 
 st.title('🏦 Loan Eligiblity Test 💵')
 
-with open("kaggle_model.pkl", "rb") as f:
-    lrc = pickle.load(f)
+# Model loading
+with open("k_model.pkl", "rb") as f:
+    model = pickle.load(f)
 
+# Assigning variables
 age = st.slider("**Age**", 20, 144, 30)
 gender = st.radio("**Gender**", ("Male", "Female"))
 education = st.selectbox("**Education**", ('High school', 'Associate', 'Bachelor', 'Master', 'Doctorate'), placeholder = "Select your educational qualification", index = None)
@@ -21,6 +23,7 @@ cbl = st.number_input("**Credit History Length**", 2, 30, help = "Credit Bureau'
 credit_score = st.number_input("**Credit score**", 390, 850)
 previous_defaults = st.radio("**Black mark**", ("Yes", "No"), help = "Previous loan repay faliure on record")
 
+# converting to a dictionary
 dict_input = {
     'person_age' : age,
     'person_gender' : gender,
@@ -36,14 +39,14 @@ dict_input = {
     'previous_loan_defaults_on_file' : previous_defaults
     }
 
+# converting to a table
 df_input = pd.DataFrame([dict_input])
 
-# st.write("From dictonary")
-# st.dataframe(df_input)
-
+# Creating dummy columns for object columns
 object_columns = df_input.select_dtypes(include = ["object"]).columns
 df_dummies = pd.get_dummies(df_input, columns = object_columns)
 
+# Defining defalut columns names and order
 feature_columns = ['person_age', 'person_income', 'person_emp_exp', 'loan_amnt',
        'loan_int_rate', 'cb_person_cred_hist_length', 'credit_score',
        'person_gender_Male', 'person_education_Bachelor',
@@ -54,83 +57,30 @@ feature_columns = ['person_age', 'person_income', 'person_emp_exp', 'loan_amnt',
        'loan_intent_Medical', 'loan_intent_Personal', 'loan_intent_Venture',
        'previous_loan_defaults_on_file_Yes']
 
+# Reindexing columns
 df_dummies = df_dummies.reindex(columns = feature_columns, fill_value = 0)
-# st.write("after reindex")
-# st.dataframe(df_dummies)
 
-# for col in feature_columns:
-#     if col not in df_dummies.columns:
-#         df_dummies[col] = 0
-
-# st.write("after for loop 01")
-# st.dataframe(df_dummies)
-
+# Drop extra columns
 drop_first_columns = ["person_gender_Female", "person_education_Associate", "person_home_ownership_Mortage", "loan_intent_Debt consolidation", "previous_loan_defaults_on_file_No"]
 for i in drop_first_columns:
     if i in df_dummies.columns:
         df_dummies = df_dummies.drop(i, axis = 1)
 
-# st.write("after for loop 02")
-# st.dataframe(df_dummies)
-
-# st.write("after dummies")
-# st.dataframe(df_dummies)
-
-# st.write(df_dummies.columns)
-
-
-
-
-# output = lrc.predict(df_dummies)
-# st.write(output)
-
-# if output == 1:
-#     st.write("Approved")
-# else:
-#     st.write("Not approved")
-
-
-# # **Predict Button**
-# if st.button("Predict"):
-#     # Make Predictions
-#     output = lrc.predict(df_dummies)[0]
-#     probabilities = lrc.predict_proba(df_dummies)[0]  # Get Probabilities for both classes
-
-#     # Display Result
-#     if output == 1:
-#         st.success("✅ **Approved**")
-#     else:
-#         st.error("❌ **Not Approved**")
-
-#     # **Probability Bar Chart**
-#     st.write("### Prediction Probability")
-#     fig, ax = plt.subplots()
-#     ax.bar(["Not Approved (0)", "Approved (1)"], probabilities, color=['red', 'green'])
-#     ax.set_ylim(0, 1)
-#     ax.set_ylabel("Probability")
-#     st.pyplot(fig)
-
-#     output_placeholder = st.empty()  # Creates an empty placeholder
-
-#     if st.button("Clear"):
-#         output_placeholder.empty()  # Clears the displayed prediction
-
-
 st.write("")  # Adds a blank line
 st.write("")  # Adds another blank line
 
-# Center align buttons
+# Center align predict button
 col1, col2, col3 = st.columns([1, 2, 1])
-
 with col2:
     predict_button = st.button("**Predict**", use_container_width=True)
 
-st.write("")  # Adds a blank line
+st.write("")
 
 if predict_button:
+
     # Make predictions
-    output = lrc.predict(df_dummies)
-    probabilities = lrc.predict_proba(df_dummies)[0]  # Get probability scores for both classes
+    output = model.predict(df_dummies)
+    probabilities = model.predict_proba(df_dummies)[0]  # Get probability scores for both classes
 
     # Display result
     if output == 1:
@@ -160,6 +110,7 @@ if predict_button:
     
     st.write("")  # Adds a blank line
 
+    # Center align retry button
     col1, col2, col3 = st.columns([1, 2, 1])
     with col2:
         clear_button = st.button("**Clear**", use_container_width=True)
